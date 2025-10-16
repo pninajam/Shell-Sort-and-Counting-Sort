828d1e000be401ad8a0af2d6e73b3e63b4151fab
pninajam <pninajam@unjbg.edu.pe>
2025-10-16 12:23:45 -0500

    Creando el menu de opciones (para la cantidad de datos)

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,10 @@ using namespace std::chrono;
 
 int main(){
     system("mkdir results");
+    int opcion, repeticiones;
+    string archivo;
 
+    cout << "Elige la cantidad de datos a usar:\n";
+    cout << "1. 10\n2. 100\n3. 1000\n4. 10000\n5. 100000\nOpcion: ";
+    cin >> opcion;
 }
