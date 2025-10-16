8e9c08e2987d191760ed530ee9e7060447691e39
pninajam <pninajam@unjbg.edu.pe>
2025-10-16 12:22:00 -0500

    Agregando las librerias necesarias

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <fstream>
 using namespace std;
+using namespace std::chrono;
 
 int main(){
-    
+    system("mkdir results");
+
 }
